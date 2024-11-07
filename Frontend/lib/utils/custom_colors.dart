a031039416e619140679bc1716f6cdd9d438d575
nhat191024 <richberchannel01@gmail.com>
2024-11-07 10:33:40 +0700

    feat: add CustomColors utility for managing color constants

---
+++ Frontend/lib/utils/custom_colors.dart
@@ -0,0 +1,24 @@
+import 'package:nega_lms/utils/imports.dart';
+
+class CustomColors {
+  // primary color
+  static const Color primary = Color(0xFF42B883);
+
+  //Neutral color
+  static const Color primaryText = Color(0xFF111111);
+  static const Color secondaryText = Color(0xFF646464);
+  static const Color disable = Color(0xFFA7A7A7);
+  static const Color border = Color(0xFFDEDEDE);
+  static const Color dividers = Color(0xFFEFEFEF);
+  static const Color background = Color(0xFFF6F6F6);
+  static const Color footer = Color(0xFF333333);
+  static const Color white = Color(0xFFFFFFFF);
+
+  //Functional color
+  static const Color errorMain = Color(0xFFE82C36);
+  static const Color errorLight = Color(0xFFFFF1F2);
+  static const Color infoMain = Color(0xFF1371DC);
+  static const Color infoLight = Color(0xFFEEF9FF);
+  static const Color successMain = Color(0xFF54B12A);
+  static const Color successLight = Color(0xFFF1FBEA);
+}
