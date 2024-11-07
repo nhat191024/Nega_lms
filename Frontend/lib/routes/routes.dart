1e21ec809294202c7565a9453f0061effe482d43
nhat191024 <richberchannel01@gmail.com>
2024-11-07 10:33:17 +0700

    feat: add routing configuration for home page paths

---
+++ Frontend/lib/routes/routes.dart
@@ -0,0 +1,11 @@
+part of 'pages.dart';
+
+abstract class Routes {
+  Routes._();
+
+  static const homePage = _Paths.homePage;
+}
+
+abstract class _Paths {
+  static const homePage = '/home-page';
+}
