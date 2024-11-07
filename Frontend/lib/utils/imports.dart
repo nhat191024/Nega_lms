fc088492ce398a8d7e32efda13f51c9921975259
nhat191024 <richberchannel01@gmail.com>
2024-11-07 10:33:47 +0700

    feat: add imports utility for centralized management of screen, controller, binding, and component imports

---
+++ Frontend/lib/utils/imports.dart
@@ -0,0 +1,30 @@
+export 'package:nega_lms/screens/my_app_screen.dart';
+
+//basic imports
+export 'dart:async';
+export 'package:flutter/material.dart';
+
+//screen imports
+export 'package:nega_lms/screens/home_screen.dart';
+
+//controller imports
+export 'package:nega_lms/controllers/splash_controller.dart';
+export 'package:nega_lms/controllers/home_controller.dart';
+
+//binding imports
+export 'package:nega_lms/bindings/home_binding.dart';
+
+//utils imports
+export 'package:nega_lms/routes/pages.dart';
+export 'package:nega_lms/utils/custom_colors.dart';
+export 'package:nega_lms/utils/text.dart';
+export 'package:nega_lms/utils/images.dart';
+
+//components imports
+export 'package:nega_lms/components/nav.dart';
+export 'package:nega_lms/components/button.dart';
+
+//package imports
+export 'package:get/get.dart';
+export 'package:flutter_svg/svg.dart';
+export 'package:flutter_web_plugins/flutter_web_plugins.dart';
