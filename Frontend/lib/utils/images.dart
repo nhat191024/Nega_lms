c84b69fd40c5dd8dc044d56bcb4464ef0ff43cf5
nhat191024 <richberchannel01@gmail.com>
2024-11-07 10:33:44 +0700

    feat: add Images utility for managing image and icon assets

---
+++ Frontend/lib/utils/images.dart
@@ -0,0 +1,9 @@
+class Images {
+  static const String logo = 'assets/images/logo.png';
+  static const String logoNoBg = 'assets/images/logo-no-bg.png';
+  static const String homeBanner1 = 'assets/images/home-banner-1.png';
+
+  //icons
+  static const String searchIcon = 'assets/icons/search.svg';
+  static const String penIcon = 'assets/icons/pen.svg'; 
+}
