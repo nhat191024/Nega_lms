1fde0923b308c4fd406b7d1d26fe089f5b25fa8f
nhat191024 <richberchannel01@gmail.com>
2024-11-10 11:25:21 +0700

    fix: rename 'question' to 'title' and add 'description' in questions table migration

--- Backend/database/migrations/2024_11_10_010632_create_questions_table.php
+++ Backend/database/migrations/2024_11_10_010632_create_questions_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('assignment_id');
-            $table->text('question');
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
