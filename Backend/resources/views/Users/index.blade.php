955f74184e229ad3003f3381fc50426a2b71e059
nhat191024 <richberchannel01@gmail.com>
2024-11-14 16:41:30 +0700

    refactor: enhance user index view layout and improve accessibility

--- Backend/resources/views/Users/index.blade.php
+++ Backend/resources/views/Users/index.blade.php
@@ -1,102 +1,84 @@
 @extends('master')
-
-@section('title')
-    <title>Quản trị người dùng</title>
-@endsection
-
+@section('title', 'Người dùng')
 @section('content')
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
-
-        <!-- Main Content -->
-        <div id="content">
-
-            <!-- Begin Page Content -->
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <h1 class="h3 mb-2 text-gray-800">Bảng người dùng</h1>
-
-                <!-- Hiển thị thông báo thành công -->
-                @if (session('success'))
-                    <div class="alert alert-success">
-                        {{ session('success') }}
-                    </div>
-                @endif
-
-                <button class="btn btn-success btn-icon-split mb-4">
-                    <span class="icon text-white-50">
-                        <i class="fas fa-plus"></i>
-                    </span>
-                    <a href={{ route('users.create') }} class="text-white">
-                        <span class="text">Tạo người dùng</span>
+    <div id="content">
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <h1 class="h3 mb-2 text-gray-800">Quản lý người dùng</h1>
+            <div class="card shadow my-4">
+                <div class="card-header py-3">
+                    <a href={{ route('users.create') }} class="btn btn-primary">
+                        <span class="text">Thêm người dùng</span>
                     </a>
-                </button>
-
-                <!-- DataTales Example -->
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Bảng Dữ Liệu Người Dùng</h6>
-                    </div>
+                </div>
+                @if ($users->isNotEmpty())
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <table class="table table-bordered class-table" data-class-table="table-users" id="table-users"
+                                width="100%" cellspacing="0">
                                 <thead>
                                     <tr>
-                                        <th>Tên</th>
-                                        <th>Email</th>
-                                        <th>Tên người dùng</th>
-                                        <th>Vai trò</th>
-                                        <th>Trạng thái</th>
-                                        <th>Hành động</th>
+                                        <th class="text-center">STT</th>
+                                        <th class="text-center">Tên</th>
+                                        <th class="text-center">Email</th>
+                                        <th class="text-center">Tên người dùng</th>
+                                        <th class="text-center">Vai trò</th>
+                                        <th class="text-center">Trạng thái</th>
+                                        <th class="text-center">Hành động</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @foreach ($users as $user)
                                         <tr>
-                                            <td>{{ $user->name }}</td>
-                                            <td>{{ $user->email }}</td>
-                                            <td>{{ $user->username }}</td>
-                                            <td>{{ $roles[$user->role_id] ?? 'Unknown' }}</td>
-                                            <td>{{ $user->status ? 'Hiển thị' : 'Ẩn' }}</td>
-                                            <td>
+                                            <td class="text-center">{{ $loop->iteration }}</td>
+                                            <td class="text-center">{{ $user->name }}</td>
+                                            <td class="text-center">{{ $user->email }}</td>
+                                            <td class="text-center">{{ $user->username }}</td>
+                                            <td class="text-center">{{ $roles[$user->role_id] }}</td>
+                                            <td class="text-center">
+                                                @if ($user->status == 1)
+                                                    <span class="badge badge-success fs-6">Hiển thị</span>
+                                                @else
+                                                    <span class="badge badge-danger fs-6">Ẩn</span>
+                                                @endif
+                                            </td>
+                                            <th class="text-center">
                                                 <a href="{{ route('users.edit', ['id' => $user->id]) }}"
-                                                    class="btn btn-warning btn-sm">Sửa</a>
+                                                    class="btn btn-warning btn-sm">
+                                                    Sửa
+                                                </a>
                                                 <a href="{{ route('users.status', ['id' => $user->id]) }}"
-                                                    class="btn 
-                                                    {{ $user->status ? 'btn-danger' : 'btn-success' }} btn-sm">
+                                                    class="btn {{ $user->status ? 'btn-danger' : 'btn-success' }} btn-sm">
                                                     @if ($user->status)
                                                         Ẩn
                                                     @else
                                                         Hiển thị
                                                     @endif
                                                 </a>
-                                            </td>
+                                            </th>
                                         </tr>
                                     @endforeach
                                 </tbody>
-                                <tfoot></tfoot>
+                                <tfoot>
+                                    <tr>
+                                        <th class="text-center">STT</th>
+                                        <th class="text-center">Tên</th>
+                                        <th class="text-center">Email</th>
+                                        <th class="text-center">Tên người dùng</th>
+                                        <th class="text-center">Vai trò</th>
+                                        <th class="text-center">Trạng thái</th>
+                                        <th class="text-center">Hành động</th>
+                                    </tr>
+                                </tfoot>
                             </table>
                         </div>
                     </div>
-                </div>
-
+                @else
+                    <div>Không có học sinh nào</div>
+                @endif
             </div>
-            <!-- /.container-fluid -->
-
         </div>
-        <!-- End of Main Content -->
-
-        <!-- Footer -->
-        <footer class="sticky-footer bg-white">
-            <div class="container my-auto">
-                <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Your Website 2020</span>
-                </div>
-            </div>
-        </footer>
-        <!-- End of Footer -->
-
+        <!-- /.container-fluid -->
     </div>
-    <!-- End of Content Wrapper -->
 @endsection
