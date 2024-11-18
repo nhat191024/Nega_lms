0186ea07ef9c9acdf124f292c470acb3d181756f
nhat191024 <richberchannel01@gmail.com>
2024-11-18 10:02:24 +0700

    refactor: Update route methods for removing students and hiding classes for clarity

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 Route::prefix(('/class'))->name('classes.')->group(function () {
     Route::get('/', [ClassController::class, 'index'])->name('index');
     Route::post('/add-student', [ClassController::class, 'addStudentToClass'])->name('addStudent');
-    Route::delete('/remove-student/{class_id}/{student_id}', [ClassController::class, 'removeStudentFormAClass'])->name('removeStudent');
+    Route::delete('/remove-student/{class_id}/{student_id}', [ClassController::class, 'removeStudentFromAClass'])->name('removeStudent');
     Route::post('/add-class', [ClassController::class, 'addNewClass'])->name('addClass');
-    Route::post('/hide-class', [ClassController::class, 'hideClassFormWebsite'])->name('hideClass');
+    Route::get('/hide-class/{class_id}', [ClassController::class, 'hideClass'])->name('hideClass');
 });
