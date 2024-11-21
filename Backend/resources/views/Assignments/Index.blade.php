80be633ddc7134d9e189e286294dd61166516cb3
thinh11os <dangcoi12os@gmail.com>
2024-11-21 11:55:32 +0700

    refactor: sửa index

--- Backend/resources/views/Assignments/Index.blade.php
+++ Backend/resources/views/Assignments/Index.blade.php
@@ -14,15 +14,26 @@
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table class="table table-bordered" width="100%" cellspacing="0">
+                                <table class="table table-bordered class-table" 
+                                       data-class-table="table-{{ Str::slug($assignments->first()->class->class_name) }}"
+                                       id="table-{{ Str::slug($assignments->first()->class->class_name) }}" 
+                                       width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
                                             <th class="text-center">STT</th>
                                             <th class="text-center">Tên lớp</th>
                                             <th class="text-center">Tiêu đề</th>
                                             <th class="text-center">Mô tả</th>
+                                            <th class="text-center">status</th>
+                                            <th class="text-center">level</th>
+                                            <th class="text-center">duration</th>
+                                            <th class="text-center">totalScore</th>
+                                            <th class="text-center">specialized</th>
+                                            <th class="text-center">subject</th>
+                                            <th class="text-center">topic</th>
+                                            <th class="text-center">start_date</th>
                                             <th class="text-center">Ngày hết hạn</th>
-                                            <th class="text-center">tự động chấm điểm</th>
+                                            <th class="text-center">Tự động chấm điểm</th>
                                             <th class="text-center">Tác vụ</th>
                                         </tr>
                                     </thead>
@@ -30,9 +41,10 @@
                                         @foreach ($assignments as $index => $assignment)
                                             <tr>
                                                 <td class="text-center">{{ $index + 1 }}</td>
-                                                <td class="text-center">{{ $assignments->first()->class->class_name }}</td>
+                                                <td class="text-center">{{ $assignment->class->class_name }}</td>
                                                 <td class="text-center">{{ $assignment->title }}</td>
                                                 <td class="text-center">{{ $assignment->description }}</td>
+                                                td class="text-center">{{ $assignment->status }}</td>
                                                 <td class="text-center">{{ $assignment->due_date }}</td>
                                                 <td class="text-center">{{ $assignment->auto_grade }}</td>
                                                 <td class="text-center">
@@ -46,6 +58,25 @@
                                             </tr>
                                         @endforeach
                                     </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th class="text-center">STT</th>
+                                            <th class="text-center">Tên lớp</th>
+                                            <th class="text-center">Tiêu đề</th>
+                                            <th class="text-center">Mô tả</th>
+                                            <th class="text-center">status</th>
+                                            <th class="text-center">level</th>
+                                            <th class="text-center">duration</th>
+                                            <th class="text-center">totalScore</th>
+                                            <th class="text-center">specialized</th>
+                                            <th class="text-center">subject</th>
+                                            <th class="text-center">topic</th>
+                                            <th class="text-center">start_date</th>
+                                            <th class="text-center">Ngày hết hạn</th>
+                                            <th class="text-center">Tự động chấm điểm</th>
+                                            <th class="text-center">Tác vụ</th>
+                                        </tr>
+                                    </tfoot>
                                 </table>
                             </div>
                         </div>
