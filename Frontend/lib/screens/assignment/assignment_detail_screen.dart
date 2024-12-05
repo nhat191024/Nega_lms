7d6c786993e1af3a48b697eaaf08986d0f47a494
nhat191024 <richberchannel01@gmail.com>
2024-12-05 14:01:59 +0700

    feat: enhance AssignmentDetailScreen layout with custom navbar and detailed information section

--- Frontend/lib/screens/assignment/assignment_detail_screen.dart
+++ Frontend/lib/screens/assignment/assignment_detail_screen.dart
@@ -7,10 +7,47 @@ class AssignmentDetailScreen extends GetView<AssignmentController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Assignment Detail'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const NavBar(),
       ),
-      body: const Center(
-        child: Text('Assignment Detail'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(100, 60, 200, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: CustomColors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Thông tin bài thi",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: CustomColors.primaryText,
+                            fontFamily: FontStyleTextStrings.medium,
+                          ),
+                        ),
+                        Row(
+                          children: [Column()],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Footer(),
+          ],
+        ),
       ),
     );
   }
