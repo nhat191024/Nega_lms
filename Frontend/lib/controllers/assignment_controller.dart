3dcfe8f9ac074c7fcc5892a443c6393da6304d93
nhat191024 <richberchannel01@gmail.com>
2024-12-05 10:04:48 +0700

    feat: add AssignmentController with search functionality and loading state

---
+++ Frontend/lib/controllers/assignment_controller.dart
@@ -0,0 +1,6 @@
+import 'package:nega_lms/utils/imports.dart';
+
+class AssignmentController extends GetxController {
+  final TextEditingController searchController = TextEditingController();
+  RxBool isLoading = true.obs;
+}
