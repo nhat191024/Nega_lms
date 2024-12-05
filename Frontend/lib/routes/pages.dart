1612eb074b997779e4015ac546859027183b3c26
nhat191024 <richberchannel01@gmail.com>
2024-12-05 10:05:04 +0700

    feat: update initial route to classListPage and add routes for login and class list screens

--- Frontend/lib/routes/pages.dart
+++ Frontend/lib/routes/pages.dart
@@ -5,13 +5,23 @@ part 'routes.dart';
 class Pages {
   Pages._();
 
-  static const initialRoute = Routes.homePage;
+  static const initialRoute = Routes.classListPage;
 
   static final routes = [
     GetPage(
       name: _Paths.homePage,
       page: () => const HomeScreen(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.loginPage,
+      page: () => const LoginScreen(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.classListPage,
+      page: () => const ClassListScreen(),
+      binding: ClassBinding(),
     ),
   ];
 }
