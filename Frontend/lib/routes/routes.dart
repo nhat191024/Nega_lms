90b4760777bb0486bcf1fedd4b97ec40b619d11e
nhat191024 <richberchannel01@gmail.com>
2024-12-05 14:01:56 +0700

    feat: add routes for assignment list and do assignment screens

--- Frontend/lib/routes/routes.dart
+++ Frontend/lib/routes/routes.dart
@@ -6,10 +6,14 @@ abstract class Routes {
   static const homePage = _Paths.homePage;
   static const loginPage = _Paths.loginPage;
   static const classListPage = _Paths.classListPage;
+  static const assignmentListPage = _Paths.assignmentListPage;
+  static const doAssignmentScreen = _Paths.doAssignmentScreen;
 }
 
 abstract class _Paths {
   static const homePage = '/';
   static const loginPage = '/login';
   static const classListPage = '/class-list';
+  static const assignmentListPage = '/assignment-list/:class_id';
+  static const doAssignmentScreen = '/do-assignment/:assignment_id';
 }
