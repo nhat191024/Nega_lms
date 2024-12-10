b4026868d5886fb81230d33c60aad0b6cacfed71
nhat191024 <richberchannel01@gmail.com>
2024-12-10 22:16:12 +0700

    feat: create Homework migration with class_id, assignment_id, and status fields

---
+++ Backend/database/migrations/2024_12_10_145739_create_homework_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('homework', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('class_id');
+            $table->enum('type', ['homework', 'quiz']);
+            $table->unsignedBigInteger('assignment_id');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('homework');
+    }
+};
