e524a1c300a2dad5fb3da3aff66eeef06fbc5469
nhat191024 <richberchannel01@gmail.com>
2024-12-10 22:15:59 +0700

    feat: refactor Assignment model to remove class_id and establish relationship with Homework

--- Backend/app/Models/Assignment.php
+++ Backend/app/Models/Assignment.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Assignment extends Model
 {
     protected $fillable = [
-        'class_id',
         'creator_id',
         'name',
         'description',
@@ -23,9 +22,9 @@ class Assignment extends Model
         'auto_grade',
     ];
 
-    public function class()
+    public function homeworks()
     {
-        return $this->belongsTo(Classes::class, 'class_id');
+        return $this->hasMany(Homework::class);
     }
 
     public function creator()
