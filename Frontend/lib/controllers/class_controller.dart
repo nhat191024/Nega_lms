831a2dd246d0343707f45d1f91249d684fa16330
nhat191024 <richberchannel01@gmail.com>
2024-12-20 04:39:27 +0700

    feat: implement joinClass and classFilter methods in ClassController for class enrollment and searching

--- Frontend/lib/controllers/class_controller.dart
+++ Frontend/lib/controllers/class_controller.dart
@@ -3,7 +3,9 @@ import 'package:nega_lms/utils/imports.dart';
 class ClassController extends GetxController {
   final TextEditingController searchController = TextEditingController();
   RxList<ClassModel> classList = <ClassModel>[].obs;
+  RxList<ClassModel> filteredList = <ClassModel>[].obs;
   RxBool isLoading = true.obs;
+  RxBool isJoinBtnLoading = false.obs;
   RxString token = "".obs;
 
   @override
@@ -13,6 +15,7 @@ class ClassController extends GetxController {
       if (!await Token.checkToken()) return;
       token.value = await Token.getToken();
       await fetchClass();
+      filteredList.value = classList;
     });
   }
 
@@ -33,7 +36,73 @@ class ClassController extends GetxController {
     }
   }
 
-  classFilter(String query) {
-    return classList.where((element) => element.name!.toLowerCase().contains(query.toLowerCase())).toList();
+  joinClass(int classId) async {
+    try {
+      isJoinBtnLoading.value = true;
+      String url = "${Api.server}classes/join/$classId";
+      var response = await get(Uri.parse(url), headers: {
+        'Authorization': 'Bearer $token',
+      }).timeout(const Duration(seconds: Api.apiTimeOut));
+
+      var data = jsonDecode(response.body);
+
+      if (response.statusCode == 201) {
+        isJoinBtnLoading.value = false;
+        Get.dialog(
+          NotificationDialogWithoutButton(
+            title: 'Thành công',
+            message: data['message'],
+          ),
+        );
+
+        await fetchClass();
+      } else if (response.statusCode == 409) {
+        isJoinBtnLoading.value = false;
+        Get.dialog(
+          NotificationDialogWithoutButton(
+            title: 'Thất bại',
+            message: data['message'],
+          ),
+        );
+      }
+    }
+    catch (e) {
+      isJoinBtnLoading.value = false;
+      Get.dialog(
+        const NotificationDialogWithoutButton(
+          title: 'Thất bại',
+          message: 'Đã có lỗi xảy ra, vui lòng thử lại sau',
+        ),
+      );
+    }
+    finally {
+      isJoinBtnLoading.value = false;
+    }
+  }
+
+  classFilter(String query) async {
+    try {
+      isLoading.value = true;
+      String url = "${Api.server}classes/search/$query";
+      var response = await get(Uri.parse(url), headers: {
+        'Authorization': 'Bearer $token',
+      }).timeout(const Duration(seconds: Api.apiTimeOut));
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        filteredList.value = (data['classes'] as List).map((e) => ClassModel.fromJson(e)).toList();
+      } else if (response.statusCode == 404) {
+        filteredList.value = classList.where((element) => element.name!.toLowerCase().contains(query.toLowerCase())).toList();
+      } else {
+        Get.dialog(
+          const NotificationDialogWithoutButton(
+            title: 'Thất bại',
+            message: 'Đã có lỗi xảy ra, vui lòng thử lại sau',
+          ),
+        );
+      }
+    } finally {
+      isLoading.value = false;
+    }
   }
 }
