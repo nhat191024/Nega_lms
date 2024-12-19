370fb3fde0f8f8fa6542c9af1986332f4f00b33f
nhat191024 <richberchannel01@gmail.com>
2024-12-20 03:21:18 +0700

    feat: add joinClass and searchClassByCode methods to ClassController

--- Backend/app/Http/Controllers/Api/V1/ClassController.php
+++ Backend/app/Http/Controllers/Api/V1/ClassController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Models\Classes;
 use App\Models\Enrollment;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class ClassController extends Controller
 {
@@ -26,7 +28,7 @@ class ClassController extends Controller
 
         return response()->json([
             'classes' => $classes,
-        ], 200);
+        ], Response::HTTP_OK);
     }
 
     public function getStudentClasses()
@@ -48,5 +50,44 @@ class ClassController extends Controller
         });
 
         return response()->json($classes, 200);
+    }
+
+    public function joinClass($classId)
+    {
+        $user = Auth::user();
+
+        $enrollment = Enrollment::where('student_id', $user->id)->where('class_id', $classId)->first();
+
+        if ($enrollment) {
+            return response()->json(['message' => 'Bạn đã tham gia lớp học này.'], Response::HTTP_CONFLICT);
+        }
+
+        $enrollment = new Enrollment();
+        $enrollment->student_id = $user->id;
+        $enrollment->class_id = $classId;
+        $enrollment->save();
+
+        return response()->json(['message' => 'Tham gia lớp học thành công.'], Response::HTTP_CREATED);
+    }
+
+    public function searchClassByCode($classCode)
+    {
+        $class = Classes::where('class_code', $classCode)->first();
+
+        if (!$class) {
+            return response()->json(['message' => 'Không tìm thấy lớp học.'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = [
+            'id' => $class->id,
+            'name' => $class->class_name,
+            'description' => $class->class_description,
+            'teacherName' => $class->teacher ? $class->teacher->name : 'Chưa có giáo viên',
+            'createdAt' => $class->created_at,
+        ];
+
+        return response()->json([
+            'classes' => [$data],
+        ], Response::HTTP_OK);
     }
 }
