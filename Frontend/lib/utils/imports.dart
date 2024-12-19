2f96a94dc661e358c35737172bd5f7990946dd01
nhat191024 <richberchannel01@gmail.com>
2024-12-19 18:37:00 +0700

    refactor: add local_storage service import for better modularity

--- Frontend/lib/utils/imports.dart
+++ Frontend/lib/utils/imports.dart
@@ -32,6 +32,9 @@ export 'package:nega_lms/utils/text.dart';
 export 'package:nega_lms/utils/images.dart';
 export 'package:nega_lms/utils/api.dart';
 
+//service imports
+export 'package:nega_lms/service/local_storage.dart';
+
 //components imports
 export 'package:nega_lms/components/nav.dart';
 export 'package:nega_lms/components/button.dart';
