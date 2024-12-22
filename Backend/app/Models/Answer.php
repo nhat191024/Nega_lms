eec51bd45bba91b84fb1b5c566806cbf9f7a6b82
nhat191024 <richberchannel01@gmail.com>
2024-12-22 21:42:52 +0700

    feat: add assignment relationship to Answer model for improved data association

--- Backend/app/Models/Answer.php
+++ Backend/app/Models/Answer.php
@@ -10,6 +10,7 @@ class Answer extends Model
         'question_id',
         'user_id',
         'choice_id',
+        'assignment_id',
         'homework_id',
         'link',
     ];
@@ -27,5 +28,15 @@ class Answer extends Model
     public function choice()
     {
         return $this->belongsTo(Choice::class);
+    }
+
+    public function homework()
+    {
+        return $this->belongsTo(Homework::class);
+    }
+
+    public function assignment()
+    {
+        return $this->belongsTo(Assignment::class);
     }
 }
