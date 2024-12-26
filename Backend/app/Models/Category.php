186882b615f0181e9d1070297ebd0b9087db1460
nhat191024 <richberchannel01@gmail.com>
2024-12-27 00:03:38 +0700

    feat: add Category model and migration for categories table

---
+++ Backend/app/Models/Category.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['name', 'parent_id'];
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function quizzes()
+    {
+        return $this->hasMany(Quiz::class);
+    }
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
+}
