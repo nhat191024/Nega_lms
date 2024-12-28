099a2cbf7ac4763b0893d7c0767c750770dc17d5
nhat191024 <richberchannel01@gmail.com>
2024-12-28 22:02:17 +0700

    feat: update DatabaseSeeder to include category seeding for QuizPackage and Classes

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -4,12 +4,16 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Role;
-use App\Models\Classes;
-use App\Models\Enrollment;
-use App\Models\Question;
+use App\Models\Category;
+use App\Models\QuizPackage;
+use App\Models\QuizPackageCategory;
+use App\Models\Quiz;
 use App\Models\Choice;
-use App\Models\Assignment;
-use App\Models\Homework;
+use App\Models\Classes;
+use App\Models\ClassCategory;
+use App\Models\ClassAssignment;
+use App\Models\AssignmentQuiz;
+use App\Models\Enrollment;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -27,40 +31,90 @@ class DatabaseSeeder extends Seeder
             Role::create($row);
         }
 
-        User::factory()->admin()->count(1)->create();
-        User::factory()->teacher()->count(4)->create();
+        User::factory()->class_supervisor()->count(1)->create();
+        User::factory()->teacher()->count(1)->create();
+        User::factory()->IT()->count(1)->create();
         User::factory()->count(10)->create();
 
-        foreach ($data['class'] as $row) {
-            Classes::create($row);
+        foreach ($data['category'] as $row) {
+            Category::create($row);
         }
 
-        foreach ($data['enrollment'] as $row) {
-            Enrollment::create($row);
-        }
-
-        foreach ($data['assignment'] as $row) {
-            Assignment::create($row);
-        }
-
-        foreach ($data['questions'] as $row) {
-            $question = Question::create([
-                'assignment_id' => $row['assignment_id'],
-                'question' => $row['question'],
-                'duration' => $row['duration'],
-                'score' => $row['score'],
+        foreach ($data['quiz_package'] as $row) {
+            $quizPackage = QuizPackage::create([
+                'creator_id' => $row['creator_id'],
+                'title' => $row['title'],
+                'description' => $row['description'],
+                'status' => $row['status'],
+                'quiz_id_range' => $row['quiz_id_range'],
             ]);
-            foreach ($row['choices'] as $choice) {
-                Choice::create([
-                    'question_id' => $question->id,
-                    'choice' => $choice['choice'],
-                    'is_correct' => $choice['is_correct'],
+
+            foreach ($row['categories'] as $category) {
+                QuizPackageCategory::create([
+                    'quiz_package_id' => $quizPackage->id,
+                    'category_id' => $category['category_id'],
                 ]);
+            }
+
+            foreach ($row['quizzes'] as $quiz) {
+                $quiz = Quiz::create([
+                    'quiz_package_id' => $quizPackage->id,
+                    'question' => $quiz['question'],
+                ]);
+
+                foreach ($quiz['choices'] as $choice) {
+                    Choice::create([
+                        'quiz_id' => $quiz->id,
+                        'choice' => $choice['choice'],
+                        'is_correct' => $choice['is_correct'],
+                    ]);
+                }
             }
         }
 
-        foreach ($data['homework'] as $row) {
-            Homework::create($row);
+        foreach ($data['class'] as $row) {
+            $class = Classes::create([
+                'code' => $row['code'],
+                'name' => $row['name'],
+                'description' => $row['description'],
+                'teacher_id' => $row['teacher_id'],
+                'status' => $row['status'],
+            ]);
+
+            foreach ($row['categories'] as $category) {
+                ClassCategory::create([
+                    'class_id' => $class->id,
+                    'category_id' => $category['category_id'],
+                ]);
+            }
+
+            foreach ($row['assignments'] as $assignment) {
+                ClassAssignment::create([
+                    'class_id' => $class->id,
+                    'type' => $assignment['type'],
+                    'title' => $assignment['title'],
+                    'description' => $assignment['description'],
+                    'duration' => $assignment['duration'],
+                    'start_date' => $assignment['start_datetime'],
+                    'due_date' => $assignment['due_datetime'],
+                    'status' => $assignment['status'],
+                ]);
+                if ($assignment['type'] === 'quiz') {
+                    foreach ($assignment['quiz'] as $quiz) {
+                        AssignmentQuiz::create([
+                            'assignment_id' => $class->id,
+                            'quiz_id' => $quiz['quiz_id'],
+                        ]);
+                    }
+                }
+            }
+        }
+
+        foreach ($data['enrollment'] as $row) {
+            Enrollment::create([
+                'class_id' => $row['class_id'],
+                'student_id' => $row['student_id'],
+            ]);
         }
     }
 }
