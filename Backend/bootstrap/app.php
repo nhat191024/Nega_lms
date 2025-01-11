e549c12445d3bb6134d27bea8a8142689a45c02d
nhat191024 <richberchannel01@gmail.com>
2025-01-11 23:13:57 +0700

    feat: remove AssignmentController and Cors middleware for codebase cleanup

--- Backend/bootstrap/app.php
+++ Backend/bootstrap/app.php
@@ -19,7 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'abilities' => CheckAbilities::class,
             'ability' => CheckForAnyAbility::class,
         ]);
-        $middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
