7aeabc05039fd151e185b5cfac14957ace421610
nhat191024 <richberchannel01@gmail.com>
2025-01-12 12:23:53 +0700

    fix: update API server URL for correct endpoint access

--- Frontend/lib/utils/api.dart
+++ Frontend/lib/utils/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static const server = 'http://127.0.0.1:8000/api/';
+  static const server = 'http://192.168.2.81:8000/api/';
   static const apiTimeOut = 10;
   static const testToken = "3|3wCvYZw6uV7m92dvn15IZbWZ6WdwSg9gdm3PxX1ded9e41d9";
 }
