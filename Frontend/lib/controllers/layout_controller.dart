8c0978ec28954c9a84dfa8fbd0e58c0376bb44fb
nhat191024 <richberchannel01@gmail.com>
2025-01-12 12:23:15 +0700

    feat: add role management and class selection handling in LayoutController

--- Frontend/lib/controllers/layout_controller.dart
+++ Frontend/lib/controllers/layout_controller.dart
@@ -3,16 +3,21 @@ import 'package:nega_lms/utils/imports.dart';
 class LayoutController extends GetxController {
   late PageController pageController;
   late SidebarXController sidebarController;
+  final RxString role = "".obs;
+  final RxInt selectedClassId = 0.obs;
 
   @override
   void onInit() {
     pageController = PageController();
     sidebarController = SidebarXController(selectedIndex: 0, extended: false);
-
-    sidebarController.addListener(() {
-      pageController.jumpToPage(sidebarController.selectedIndex);
-    });
-
+    if (StorageService.checkData(key: "role")) {
+      role.value = StorageService.readData(key: "role");
+    }
     super.onInit();
+  }
+
+  void goToClassDetail(int classId) {
+    selectedClassId.value = classId;
+    pageController.jumpToPage(1);
   }
 }
