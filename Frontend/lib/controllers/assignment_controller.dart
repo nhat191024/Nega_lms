b4259e56b8bc4fe07cfdda60f7e081c26047c8db
nhat191024 <richberchannel01@gmail.com>
2025-01-13 23:10:55 +0700

    feat: fetch student assignment after successful class assignment retrieval

--- Frontend/lib/controllers/assignment_controller.dart
+++ Frontend/lib/controllers/assignment_controller.dart
@@ -84,6 +84,7 @@ class AssignmentController extends GetxController {
 
       if (response.statusCode == 200) {
         classDetailController.fetchClassAssignment(classId.value);
+        classDetailController.fetchStudentAssignment();
         Get.back();
         await Future.delayed(const Duration(seconds: 1));
         clear();
