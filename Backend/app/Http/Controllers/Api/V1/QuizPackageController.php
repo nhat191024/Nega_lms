4e92e67d889e5faf4dde2dad70fb3c36180809a4
nhat191024 <richberchannel01@gmail.com>
2025-01-14 07:50:08 +0700

    feat: implement create method for QuizPackageController to handle quiz package creation

--- Backend/app/Http/Controllers/Api/V1/QuizPackageController.php
+++ Backend/app/Http/Controllers/Api/V1/QuizPackageController.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 use App\Models\QuizPackage;
+use App\Models\QuizPackageCategory;
+use App\Models\Quiz;
+use App\Models\Choice;
 
 class QuizPackageController extends Controller
 {
@@ -39,5 +42,41 @@ class QuizPackageController extends Controller
         });
 
         return response()->json($quizPackages, Response::HTTP_OK);
+    }
+
+    public function create(Request $request)
+    {
+        $user = Auth::user();
+        $quizPackages = QuizPackage::create([
+            'creator_id' => $user->id,
+            'title' => $request->title,
+            'description' => $request->description,
+            'quiz_id_range' => 0,
+            'status' => 'published',
+            'type' => $request->type,
+        ]);
+
+        QuizPackageCategory::create([
+            'quiz_package_id' => $quizPackages->id,
+            'category_id' => 1,
+        ]);
+
+        $questions = json_decode($request->questions, true);
+        foreach ($questions as $question) {
+            $quiz = Quiz::create([
+                'question' => $question['question'],
+                'quiz_package_id' => $quizPackages->id,
+            ]);
+
+            foreach ($question['choices'] as $choice) {
+                Choice::create([
+                    'quiz_id' => $quiz->id,
+                    'choice' => $choice['choice'],
+                    'is_correct' => $choice['is_correct'],
+                ]);
+            }
+        }
+
+        return response()->json('ok', Response::HTTP_CREATED);
     }
 }
